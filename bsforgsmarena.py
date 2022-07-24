f5148606bd64b20766432dbb09e06a8ee176de4e
Hasan Can Nesanır <108904271+hasancannes@users.noreply.github.com>
2022-07-24 11:49:00 +0300

    Update bsforgsmarena.py

--- bsforgsmarena.py
+++ bsforgsmarena.py
@@ -8,56 +8,85 @@ from time import sleep
 import re
 import pandas as pd
 
-header={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_***) AppleWebKit/53*** (KHTML, like Gecko) Chrome/83.****** Safari/537.***"}
-header2={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41"}
 
-for i in range(17):
-    r = rq.get(f"https://www.gsmarena.com/samsung-phones-f-9-0-p{i}.php",headers=header)
+class webScraping:
     
-    soup = bs(r.content, "lxml")
-    
-    whole_table = soup.find("div", attrs={'class':'section-body'})
-    items = whole_table.find_all("li")
-
-    head_div = soup.find("div", attrs={'class':'makers'})
-    head_li = whole_table.find_all("li")
-
-    for phones in head_li:
-        names_features = phones.img.get("title")
-        image_links = phones.img.get("src")
-        head_a = phones.find("a")
-        phones_names = head_a.find("span").text
-        features_list=re.split(",|\.",names_features)
-        for phone_pages in items:
-            items_part_link = phone_pages.a.get("href")
+    def get_adress(self):
+        header1={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_***) AppleWebKit/53*** (KHTML, like Gecko) Chrome/83.****** Safari/537.***"}
+        for i in range(17):
+            r = rq.get(f"https://www.gsmarena.com/samsung-phones-f-9-0-p{i}.php",headers=header1)
+            sleep(2)
+            soup = bs(r.content, "lxml")
+            sleep(6)
+            whole_table = soup.find("div", attrs={'class':'section-body'})
+            sleep(4)
+            items = whole_table.find_all("li")
+            sleep(3)
+            head_div = soup.find("div", attrs={'class':'makers'})
+            sleep(2)
+            self.head_li = whole_table.find_all("li")
+            sleep(3)
+            gsmarena.get_phones()
+            gsmarena.get_print_all_values()
             
+    def get_phones(self):
+        for phones in self.head_li:
+            names_features = phones.img.get("title")
+            sleep(3)
+            self.image_links = phones.img.get("src")
+            sleep(2)
+            self.head_a = phones.find("a")
+            sleep(4)
+            self.phones_names = self.head_a.find("span").text
+            sleep(5)
+            self.features_list=re.split(",|\.",names_features)
+            sleep(2)
+            gsmarena.get_phone_pages()
+
+    def get_phone_pages(self):
+        for phone_pages in self.head_li:
+            items_part_link = phone_pages.a.get("href")
+            sleep(2)
             base_link = "https://www.gsmarena.com/"
             items_whole_link = base_link+items_part_link
-            phone_page = rq.get(items_whole_link, headers=header2)
-            
+            sleep(6)
+            phone_page = rq.get(items_whole_link)
+            sleep(4)
             phone_soup = bs(phone_page.content, "lxml")
-            whole_feature_table = phone_soup.find_all("div", {"id":"specs-list"})
-            for feature in whole_feature_table:
+            sleep(3)
+            self.whole_feature_table = phone_soup.find_all("div", {"id":"specs-list"})
+            sleep(7)
+            gsmarena.get_phone_features()
+
+    def get_phone_features(self):
+        for feature in self.whole_feature_table:
                 feature_details = feature.find_all("tr")
-                
+                sleep(2)
                 for rows in feature_details:
                     try:
-                        row_name = rows.find("th", attrs={"scope":"row"}).text
-                        
+                        self.row_name = rows.find("th", attrs={"scope":"row"}).text
+                        sleep(3)    
                     except AttributeError:
                         print("-")
+                        sleep(5)
                     try:    
-                        row_head = rows.find("td", attrs={"class":"ttl"}).text
-                        
+                        self.row_head = rows.find("td", attrs={"class":"ttl"}).text
+                        sleep(2)    
                     except AttributeError:
                         print("-")
+                        sleep(4)
                     try:
-                        row_info = rows.find("td", attrs={"class":"nfo"}).text
-                        
+                        self.row_info = rows.find("td", attrs={"class":"nfo"}).text
+                        sleep(4)   
                     except AttributeError:
                         print("-")
-                    
-                    data_features= {"PhoneName": [phones_names], "HeadFeatures": [features_list[1:]],"ImageLink": [image_links], "RowHead":[row_head], "RowName": [row_name],  "RowInfo":[row_info]}
-                    excel_file = pd.DataFrame(data=data_features, index=[features_list[0]])               
-                    print(excel_file)
-excel_file.to_csv(r'C:\Users\hasan\Desktop\upwork projects\gsmarena.csv',index=False)    
+                        sleep(7)
+    def get_print_all_values(self):
+        data_features= {"PhoneName": [self.phones_names], "HeadFeatures": [self.features_list[1:]],"ImageLink": [self.image_links], "RowHead":[self.row_head], "RowName": [self.row_name],  "RowInfo":[self.row_info]}
+        sleep(4)
+        excel_file = pd.DataFrame(data=data_features, index=[self.features_list[0]])               
+        print(excel_file)
+        excel_file.to_csv(r'C:\Users\hasan\Desktop\upwork projects\gsmarena.csv',index=False)
+
+gsmarena = webScraping()
+gsmarena.get_adress()
